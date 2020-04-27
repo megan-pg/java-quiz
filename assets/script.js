71b647cc0c6317e591944c812c5c85c44594ae7f
Megan Pardy-Gokcu <meganpardy-gokcu@MPG2s-MacBook-Pro.local>
2020-04-26 20:53:29 -0400

    almost fixed choices

--- assets/script.js
+++ assets/script.js
@@ -4,31 +4,39 @@ var questions = [
     {
         question: "Who is Quark's step-father?",
         choices: ["Lt. Worf", "Rom", "Grand Negus Zek", "Liquidator Brunt"],
-        correctAnswer: "C"
+        correctAnswer: "Grand Negus Zek"
     },
     {
         question: "What alien species is Lt. Worf?",
         choices: ["Vulcan", "Klingon", "Romulan", "Betazoid"],
-        correctAnswer: "B"
+        correctAnswer: "Klingon"
     },
     {
         question: "When did Captain Sisko's wife die?",
         choices: ["At the Battle of Wolf 359", "At their wedding", "When she became a shape-shifter", "When she lied about being human"],
-        correctAnswer: "A"
+        correctAnswer: "At the Battle of Wolf 359"
     },
     {
         question: "Who does Kira hate?",
-        choices: {
-            A: "Dr. Bashir",
-            B: "Lt. Worf",
-            C: "Jadzia Dax",
-            D: "Gul Dukat"
-        },
-        correctAnswer: "D"
+        choices: ["Dr. Bashir", "Lt. Worf", "Jadzia Dax", "Gul Dukat"],
+        correctAnswer: "Gul Dukat"
     },
 ];
 var choicesText = document.querySelector("#quiz");
 var choices = document.querySelector("#choiceList");
+
+//$('#quiz').hide();
+
+function hideShowQuiz() {
+    console.log(hideShowQuiz)
+    var quizDisplay = document.getElementById("quiz");
+    if (quizDisplay.style.display === "none") {
+        startGame.style.display = "block";
+    } else {
+        quiz.style.display = "none";
+    }
+
+}
 // upon loading the document, the user is going to see the instructions to the game.
 // these instructions can just be part of the HTML. We'll wrap all of the instructions
 // in a div, with an id of #intro
@@ -36,7 +44,7 @@ var choices = document.querySelector("#choiceList");
 // ***************************** START GAME ************************************
 
 var quiz = document.getElementById("#quiz");
-var choiceList = document.querySelector("#choiceList");
+
 
 function generateQuestions(questions) {
     console.log(questions)
@@ -49,19 +57,20 @@ function generateQuestions(questions) {
     for (var i = 0; i < questions.length; i++) {
         console.log(questions.length)
         // first reset the list of answers
-        choices = [""];
+        choices = [];
 
         // for each available answer to this question...
-        for (letter in questions[i].choices) {
+        for (correctAnswer in questions[i].choices) {
 
             // ...add an html radio button
             choices.push(
-                '<label>'
-                + '<button class="choiceList" name="question' + i + '" value="' + choices + '">'
-                + choices + ': '
+                '<button>'
+                //+ '<button class="choiceList" name="question' + i + '" value="' + choices + '">'
+                //+ choices + ': '
                 + questions[i].choices
-                + '</label>'
+                + '</button>'
             );
+            console.log(choices.push)
 
         }
 
@@ -78,7 +87,7 @@ function generateQuestions(questions) {
 
 //      - append the box to the document body
 $('body').append(questions);
-$('body').append(choices);
+//$('body').append(choices);
 
 function showResults(questions, quizContainer) {
 
@@ -95,29 +104,39 @@ function showResults(questions, quizContainer) {
 
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
 
 generateQuestions(questions, quizContainer);
 
 var lastQuestionIndex = questions.length - 1;
 var runningQuestionIndex = 0;
 
+
+//*****************Time******************************* */
+var timeElement = document.querySelector("time");
+var countDown = document.getElementById(".timer");
+gameTime = 0;
+var secondsLeft = 60;
 function createTimer() {
     // store the current time in a variable called gameTime, which is going to default to 0
-    gameTime = 0;
+
     // create a setInterval, set to 1000ms (1 second), and assign that to a variable called gameTimer
     gameTimer = setInterval(function () {
         // increment the gameTime by 1 (gameTime++)
-        gameTime++;
-        console.log(gameTime);
-    }, 1000);
+        secondsleft--;
+        timeElement.textConent = secondsleft;
+        if (secondsLeft === 0) {
+            clearInterval(gameTime);
+            countDown.appendChild(timeElement);
+        }
+    }, 100);
+    console.log(gameTime)
 }
 
 // we need a start button, with an id of #start-button. We'll put this in the #intro div
 // when the #start-button is clicked, we need to start the game:
 function startGame() {
     // generate 10-20 boxes, and randomly place them on the document
-    generateQuestions();
+    generateQuestions(questions);
     // start a timer to keep track of how long the user has taken to finish the game
     createTimer();
     // hide the #intro div
@@ -128,7 +147,7 @@ $('#start-button').on('click', startGame);
 
 
 function calculateScore() {
-    return Math.round(numBoxes * 2.5) - gameTime;
+    return Math.round(correctAnswer + 10);
 }
 
 function getScores() {
@@ -182,8 +201,6 @@ function playAgain() {
     $('#score-container').hide();
     // reset the gameTime, the numOfBoxesClicked, and the numBoxes
     gameTime = 0;
-    numBoxesClicked = 0;
-    numBoxes = 0;
     // call the startGame function
     startGame();
 }
@@ -195,67 +212,4 @@ $('#play-again').on('click', playAgain);
 //   2. start the game
 
 // ************************** VIEW SCORES **************************************
-function viewScores() {
-    //   1. hide the users score, and the #play-again and #view-scores buttons
-    $('#score-container').hide();
-    //   2. get the scores from local storage
-    var scores = getScores();
-    //   3. display the scores in a table
-    var tableBodyElement = $('#score-body');
-    scores.forEach(function (score) {
-        // for each score, add a new row to the tbody, which has:
-        //   the score
-        //   the number of blocks
-        //   the game time
-        var tableRowElement = $('<tr>');
-        var scoreTd = $('<td>').text(score.score);
-        var numBoxesTd = $('<td>').text(score.numBoxes);
-        var timeTd = $('<td>').text(score.time);
-        tableRowElement.append(scoreTd);
-        tableRowElement.append(numBoxesTd);
-        tableRowElement.append(timeTd);
-        tableBodyElement.append(tableRowElement);
-    });
-    // display the #score-list div
-    $('#score-list').show();
-}
-// when the user clicks on #view-scores we need to:
-$('#view-scores').on('click', viewScores);
-
-// function generateQuestion(questions, quizContainer) {
-
-//     // we'll need a place to store the output and the answer choices
-//     var output = [];
-//     var answers;
-
-//     // for each question...
-//     for (var i = 0; i < questions.length; i++) {
-//         console.log(questions);
-//         // first reset the list of answers
-//         answers = [];
-
-//         // for each available answer to this question...
-//         for (letter in questions[i].answers) {
-
-//             // ...add an html radio button
-//             answers.push(
-//                 '<label>'
-//                 + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-//                 + letter + ': '
-//                 + questions[i].answers[letter]
-//                 + '</label>'
-//             );
-
-//         }
-
-//         // add this question and its answers to the output
-//         output.push(
-//             '<div class="question">' + questions[i].question + '</div>'
-//             + '<div class="answers">' + answers.join('') + '</div>'
-//         );
-//     }
-
-//     // finally combine our output list into one string of html and put it on the page
-//     quizContainer.innerHTML = output.join('');
-// }
 
